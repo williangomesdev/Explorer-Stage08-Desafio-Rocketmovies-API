8aee2c46574d7291c367c93b5f4d39be70dbc078
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-26 06:10:28 -0300

    Inserindo método get

--- src/server.mjs
+++ src/server.mjs
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+//método get (leitura)
+app.get("/get",(request,response)=>{
+    response.send("Resposta enviada pelo método GET")
+})
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
