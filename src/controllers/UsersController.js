efe639fb58adc575ef0895690a2adb81b1bf3ceb
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-29 14:20:00 -0300

    criação do banco de dados

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,20 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   createUser(request, response) {
     const { name, email, password, avatar } = request.body;
 
-    response.json({
+    if (!name) {
+      throw new AppError("Nome é obrigatório!");
+    }
+    if (!email) {
+      throw new AppError("Email é obrigatório!");
+    }
+    if (!password) {
+      throw new AppError("Senha é obrigatória!");
+    }
+
+    response.status(201).json({
       name,
       email,
       password,
