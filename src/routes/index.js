082f1efa80dbe2c5e87fb636632e68cdf52969bb
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-07-02 21:55:32 -0300

    creiando as rotas das tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const usersRoutes = require("./user.routes");
 const moviesRoutes = require("./movieNotes.routes");
+const tagsRoutes = require("./movieTags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/notes", moviesRoutes);
+routes.use("/tags", tagsRoutes);
 
 module.exports = routes;
